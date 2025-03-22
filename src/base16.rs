2b673957f7c3ff96b6682eb4340d77d76421a90b
Milan Müller <developement@milanmueller.de>
2025-03-22 11:49:00 +0100

    first working version

---
+++ src/base16.rs
@@ -0,0 +1,143 @@
+use cosmic_theme::palette::{Srgb, Srgba, WithAlpha};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct Base16Theme {
+    pub base00: Srgb<f32>, // Background
+    pub base01: Srgb<f32>, // Lighter background
+    pub base02: Srgb<f32>, // Selection background
+    pub base03: Srgb<f32>, // Comments, invisibles
+    pub base04: Srgb<f32>, // Dark foreground
+    pub base05: Srgb<f32>, // Default foreground
+    pub base06: Srgb<f32>, // Light foreground
+    pub base07: Srgb<f32>, // Light background
+    pub base08: Srgb<f32>, // Red
+    pub base09: Srgb<f32>, // Orange
+    pub base0a: Srgb<f32>, // Yellow
+    pub base0b: Srgb<f32>, // Green
+    pub base0c: Srgb<f32>, // Cyan
+    pub base0d: Srgb<f32>, // Blue
+    pub base0e: Srgb<f32>, // Purple
+    pub base0f: Srgb<f32>, // Brown
+}
+// Structs needed for yaml parsing
+#[derive(Debug, Deserialize, Serialize)]
+struct Base16YamlScheme {
+    system: String,
+    name: String,
+    author: String,
+    variant: String,
+    palette: Base16YamlPalette,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Base16YamlPalette {
+    base00: String,
+    base01: String,
+    base02: String,
+    base03: String,
+    base04: String,
+    base05: String,
+    base06: String,
+    base07: String,
+    base08: String,
+    base09: String,
+    #[allow(non_snake_case)]
+    base0A: String,
+    #[allow(non_snake_case)]
+    base0B: String,
+    #[allow(non_snake_case)]
+    base0C: String,
+    #[allow(non_snake_case)]
+    base0D: String,
+    #[allow(non_snake_case)]
+    base0E: String,
+    #[allow(non_snake_case)]
+    base0F: String,
+}
+
+impl Base16Theme {
+    /// Parse base16 theme from list of string.
+    /// Input is assumed to be exatly 16 strings for the grammar
+    /// digit ::= 0-9 | a-f | A-F
+    /// base16 ::= #<digit><digit><digit><digit><digit><digit>
+    /// The colors will be parsed in ascending order (i.e. from base00 to base0f).
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if the input does not satisfy the grammar above
+    /// or if there are not exactly 16 colors given.
+    pub(crate) fn from_args(args: &[String]) -> Result<Self, Box<dyn std::error::Error>> {
+        if args.len() != 16 {
+            // 16 colors + program name
+            return Err(format!("Expected 16 hex colors as arguments, got {}", args.len()).into());
+        }
+        // We can now assume args.len() == 17
+        // Verify the colors
+        let colors: Vec<Srgb<f32>> = args[0..16]
+            .into_iter()
+            .map(|arg| -> Result<Srgb<f32>, Box<dyn std::error::Error>> {
+                let color_u8 = arg.parse::<Srgb<u8>>()?;
+                Ok(color_u8.into())
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+        Ok(Base16Theme {
+            base00: colors[0],
+            base01: colors[1],
+            base02: colors[2],
+            base03: colors[3],
+            base04: colors[4],
+            base05: colors[5],
+            base06: colors[6],
+            base07: colors[7],
+            base08: colors[8],
+            base09: colors[9],
+            base0a: colors[10],
+            base0b: colors[11],
+            base0c: colors[12],
+            base0d: colors[13],
+            base0e: colors[14],
+            base0f: colors[15],
+        })
+    }
+
+    // Parse from YAML file
+    pub(crate) fn from_yaml_file(path: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let file = std::fs::File::open(path)?;
+        let theme: Base16YamlScheme = serde_yaml::from_reader(file)?;
+        let c00: Srgb<u8> = theme.palette.base00.parse::<Srgb<u8>>()?;
+        let c01: Srgb<u8> = theme.palette.base01.parse::<Srgb<u8>>()?;
+        let c02: Srgb<u8> = theme.palette.base02.parse::<Srgb<u8>>()?;
+        let c03: Srgb<u8> = theme.palette.base03.parse::<Srgb<u8>>()?;
+        let c04: Srgb<u8> = theme.palette.base04.parse::<Srgb<u8>>()?;
+        let c05: Srgb<u8> = theme.palette.base05.parse::<Srgb<u8>>()?;
+        let c06: Srgb<u8> = theme.palette.base06.parse::<Srgb<u8>>()?;
+        let c07: Srgb<u8> = theme.palette.base07.parse::<Srgb<u8>>()?;
+        let c08: Srgb<u8> = theme.palette.base08.parse::<Srgb<u8>>()?;
+        let c09: Srgb<u8> = theme.palette.base09.parse::<Srgb<u8>>()?;
+        let c0a: Srgb<u8> = theme.palette.base0A.parse::<Srgb<u8>>()?;
+        let c0b: Srgb<u8> = theme.palette.base0B.parse::<Srgb<u8>>()?;
+        let c0c: Srgb<u8> = theme.palette.base0C.parse::<Srgb<u8>>()?;
+        let c0d: Srgb<u8> = theme.palette.base0D.parse::<Srgb<u8>>()?;
+        let c0e: Srgb<u8> = theme.palette.base0E.parse::<Srgb<u8>>()?;
+        let c0f: Srgb<u8> = theme.palette.base0F.parse::<Srgb<u8>>()?;
+        Ok(Base16Theme {
+            base00: c00.into(),
+            base01: c01.into(),
+            base02: c02.into(),
+            base03: c03.into(),
+            base04: c04.into(),
+            base05: c05.into(),
+            base06: c06.into(),
+            base07: c07.into(),
+            base08: c08.into(),
+            base09: c09.into(),
+            base0a: c0a.into(),
+            base0b: c0b.into(),
+            base0c: c0c.into(),
+            base0d: c0d.into(),
+            base0e: c0e.into(),
+            base0f: c0f.into(),
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,147 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, path::Path};
+
+use base16::Base16Theme;
+use cosmic_config::CosmicConfigEntry;
+use cosmic_theme::{palette::WithAlpha, Theme, ThemeBuilder};
+
+mod base16;
+
+// Serialize arbitrary nested object into a folder, s.t. each member is serialized
+// into an individual file. (This is what cosmic-settings does when applying themes)
+macro_rules! serialize_to_folder {
+    // Base case: serialize a single struct into a folder
+    ($value:expr, $path:expr, { $($field:ident),* }) => {
+        {
+            let path = $path;
+            fs::create_dir_all(&path)?;
+
+            // Serialize each field
+            $(
+                serialize_to_folder!(@field $value.$field, path.join(stringify!($field)));
+            )*
+
+            // Ok::<(), Box<dyn Error>>(())
+        }
+    };
+
+    // Handle individual fields
+    (@field $field_value:expr, $field_path:expr) => {
+        fs::write(
+            $field_path,
+            ron::ser::to_string_pretty(
+                &$field_value,
+                ron::ser::PrettyConfig::new()
+            )?
+        )?;
+    };
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Parse Command line arguments into a base16 color scheme
+    let args: Vec<String> = std::env::args().collect();
+
+    println!("number of args: {}", args.len());
+
+    let mode_str = match args.get(1) {
+        Some(s) => s.as_str(),
+        None => {
+            eprintln!(
+                "Usage: {} light|dark <base00> <base01> ... <base0f> OR {} light|dark <yaml_file>",
+                args[0], args[0]
+            );
+            std::process::exit(1);
+        }
+    };
+    let is_dark = match mode_str {
+        "light" => false,
+        "dark" => true,
+        _ => {
+            eprintln!(
+                "Usage: {} light|dark <base00> <base01> ... <base0f> OR {} light|dark <yaml_file>",
+                args[0], args[0]
+            );
+            std::process::exit(1);
+        }
+    };
+
+    // Either parse 16 given color codes, or one given yaml file
+    let theme = match args.len() {
+        18 => Base16Theme::from_args(&args[2..])?,
+        3 => Base16Theme::from_yaml_file(&args[2])?,
+        _ => {
+            eprintln!(
+                "Usage: {} light|dark <base00> <base01> ... <base0f> OR {} light|dark <yaml_file>",
+                args[0], args[0]
+            );
+            std::process::exit(1);
+        }
+    };
+    println!("{:?}", theme);
+
+    // Create theme
+
+    // Set base colors
+    let mut builder = match is_dark {
+        true => ThemeBuilder::dark(),
+        false => ThemeBuilder::light(),
+    }
+    .bg_color(theme.base00.with_alpha(1.0_f32))
+    .accent(theme.base08)
+    .text_tint(theme.base05);
+    // Set palette
+    builder.palette.as_mut().accent_red = theme.base08.with_alpha(1.0_f32);
+    builder.palette.as_mut().accent_orange = theme.base09.with_alpha(1.0_f32);
+    builder.palette.as_mut().accent_yellow = theme.base0a.with_alpha(1.0_f32);
+    builder.palette.as_mut().accent_green = theme.base0b.with_alpha(1.0_f32);
+    builder.palette.as_mut().accent_indigo = theme.base0c.with_alpha(1.0_f32);
+    builder.palette.as_mut().accent_blue = theme.base0d.with_alpha(1.0_f32);
+    builder.palette.as_mut().accent_purple = theme.base0e.with_alpha(1.0_f32);
+    builder.palette.as_mut().accent_pink = theme.base0f.with_alpha(1.0_f32);
+
+    serialize_to_folder!(builder, Path::new("output/builder"), {
+        accent,
+        bg_color,
+        palette,
+        text_tint });
+
+    // Write Builder Config to config directory
+    // let builder_config = if is_dark {
+    //     ThemeBuilder::dark_config()?
+    // } else {
+    //     ThemeBuilder::light_config()?
+    // };
+    // builder.write_entry(&builder_config)?;
+    // Create theme from builder and write it to config files
+    let theme = builder.build();
+    serialize_to_folder!(theme, Path::new("output/theme"), {
+        accent,
+        accent_button,
+        accent_text,
+        active_hint,
+        background,
+        button,
+        destructive,
+        destructive_button,
+        icon_button,
+        is_dark,
+        is_frosted,
+        is_high_contrast,
+        link_button,
+        palette,
+        primary,
+        secondary,
+        shade,
+        success,
+        success_button,
+        text_button,
+        warning,
+        warning_button,
+        window_hint });
+    // let theme_config = if is_dark {
+    //     Theme::dark_config()?
+    // } else {
+    //     Theme::light_config()?
+    // };
+    // theme.write_entry(&theme_config)?;
+    Ok(())
 }
